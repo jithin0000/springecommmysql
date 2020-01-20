d9d44d2cac6592b3bf22e320688c135d436ba4c1
jithin0000 <mbjithinbabu@gmail.com>
2020-01-20 20:25:44 +0530

    updated authorization

--- src/main/java/com/jithin/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/jithin/ecommerce/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.jithin.ecommerce.services.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -62,6 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(
                         "/",
+                        "/api/cart/**",
                         "/**/*.png",
                         "/**/*.jpg",
                         "/**/*.svg",
@@ -72,6 +74,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 ).permitAll()
                 .antMatchers("/api/auth/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/category/**","/api/product/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class);
